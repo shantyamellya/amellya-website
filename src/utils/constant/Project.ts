192d2d6a49f92eb29a5111a5bdd153c1009db8e3
shantyamellya <santiamelia0380@gmail.com>
2024-12-17 16:43:20 +0700

    add image and video

--- src/utils/constant/Project.ts
+++ src/utils/constant/Project.ts
@@ -72,6 +72,43 @@ export const project = [
     url: '/image/18.jpeg',
   },
   {
+    type: 'image',
+    url: '/image/19.jpeg',
+  },
+  {
+    type: 'image',
+    url: '/image/20.jpeg',
+  },
+  {
+    type: 'image',
+    url: '/image/21.jpeg',
+  },
+  {
+    type: 'image',
+    url: '/image/22.jpeg',
+  },
+  {
+    type: 'image',
+    url: '/image/23.jpeg',
+  },
+  {
+    type: 'image',
+    url: '/image/24.jpeg',
+  },
+  {
+    type: 'image',
+    url: '/image/26.jpeg',
+  },
+  {
+    type: 'image',
+    url: '/image/27.jpeg',
+  },
+  {
+    type: 'image',
+    url: '/image/28.jpeg',
+  
+  },
+  {
     type: 'video',
     url: '/video/video1.mp4',
   },
@@ -83,4 +120,9 @@ export const project = [
     type: 'video',
     url: '/video/video3.mp4',
   },
+  {
+    type: 'video',
+    url: '/video/video4.mp4',
+  }
 ]
+
